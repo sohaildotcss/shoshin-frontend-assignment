17971519d72dfdd4ded2c7dc795d0232fd53f3b4
sohaildotcss <sohailqwerty7@gmail.com>
2025-01-18 13:29:10 +0530

    Refactor ActivityCard and Button components for improved styling and layout; update CardTitle class

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -3,25 +3,22 @@ import { Button } from "@/components/ui/button";
 
 const ActivityCard = () => {
 	return (
-		<Card className=" ml-24 p-6 pb-0 flex flex-col gap-8 bg-[#121843] text-white">
+		<Card className=" ml-24 p-6 pb-0  mr-6 flex flex-col gap-8 bg-[#121843] text-white">
 			<h3 className="text-lg font-medium">Recently Activity</h3>
 			<div className="flex flex-col gap-2">
-				<p className="text-xs font-medium text-gray-400">
+				<p className="text-xs font-light text-gray-400">
 					10:40 AM, Fri 10 Sept 2021
 				</p>
 				<h4 className="text-lg  font-medium">You Posted a New Job</h4>
-				<p className="text-gray-300 text-sm font-normal">
+				<p className="text-gray-300 text-sm font-light pr-20">
 					Kindly check the requirements and terms of work and make sure
 					everything is right.
 				</p>
 			</div>
 
 			<div className="flex justify-between items-center">
-				<p className="text-md font-light">Today you makes 12 Activity</p>
-				<Button
-					variant="secondary"
-					className="bg-[#FF5151] text-md text-white mr-9"
-				>
+				<p className="text-sm font-light">Today you makes 12 Activity</p>
+				<Button variant="secondary" className="text-md text-white ">
 					See All Activity
 				</Button>
 			</div>
